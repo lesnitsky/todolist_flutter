5e0c5a655937684a55e31a550367e58a9c21ff01
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2018-12-04 18:48:13 +0300

    ## Render list
    Basic statefull widget will look like this

---
+++ lib/todo_list.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TodoList extends StatefulWidget {
+  @override
+  _TodoListState createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
