7153c793d0b96eec390c39778ead6c22e6a34df3
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2018-12-04 18:51:42 +0300

    We also need to import our `TodoList` widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'package:todo_list/todo_list.dart';
+
 void main() {
   SystemChrome.setEnabledSystemUIOverlays([]);
   runApp(MyApp());
