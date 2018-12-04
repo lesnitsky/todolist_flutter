b7cc066fea84a5ad24c016f5d18e650740c26a2a
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2018-12-04 18:39:39 +0300

    Now we need to add an application header which will display our app title

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Todo List',
       home: Scaffold(
+        appBar: AppBar(title: Text('Todo List')),
       ),
     );
   }
