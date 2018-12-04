fec5a7d9f43cf06dfcb110c7b911eedc32d4cf74
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2018-12-05 01:25:51 +0300

    Let's move list related logic to a separate stateless widget

---
+++ lib/todo_list.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class TodoList extends StatelessWidget {
+  _toggleTodo(Todo todo, bool isChecked) {
+    setState(() {
+      todo.isDone = isChecked;
+    });
+  }
+
+  Widget _buildItem(BuildContext context, int index) {
+    final todo = todos[index];
+
+    return CheckboxListTile(
+      value: todo.isDone,
+      title: Text(todo.title),
+      onChanged: (bool isChecked) {
+        _toggleTodo(todo, isChecked);
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: _buildItem,
+      itemCount: todos.length,
+    );
+  }
+}
