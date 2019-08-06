238e3f696d9fe0f74171db7b98df842d197acf9d
soonsam123 <karatesoon@gmail.com>
2019-08-06 07:16:07 -0300

    Adding linethrough when item is done

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -10,12 +10,22 @@ class TodoList extends StatelessWidget {
   final List<Todo> todos;
   final ToggleTodoCallback onTodoToggle;
 
+  TextStyle _getTextStyle(BuildContext context, bool isDone) {
+    if (!isDone) return null;
+
+    return TextStyle(
+        color: Colors.black54, decoration: TextDecoration.lineThrough);
+  }
+
   Widget _buildItem(BuildContext context, int index) {
     final todo = todos[index];
 
     return CheckboxListTile(
       value: todo.isDone,
-      title: Text(todo.title),
+      title: Text(
+        todo.title,
+        style: _getTextStyle(context, todo.isDone),
+      ),
       onChanged: (bool isChecked) {
         onTodoToggle(todo, isChecked);
       },
